a1ed23c3a10307567aae3eeb8449ff6625ae4dcf
Spenser Reinhardt <spenser.reinhardt@honeyapps.net>
2021-12-28 11:14:30 -0500

    finish up hello world

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 #![no_std]
+use winapi::km::wdm::{DbgPrint, DRIVER_OBJECT};
+use winapi::shared::ntdef::{NTSTATUS, NT_SUCCESS, UNICODE_STRING};
+use winapi::shared::ntstatus::STATUS_SUCCESS;
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
@@ -6,6 +9,20 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 }
 
 #[no_mangle]
-pub extern "system" fn driver_entry() -> u32 {
-    0 /* NT_STATUS_SUCCESS */
+pub extern "system" fn driver_entry(driver: &mut DRIVER_OBJECT, _: *const UNICODE_STRING)
+    -> NTSTATUS
+{
+    unsafe {
+        DbgPrint("Hello, from Rust!\0".as_ptr());
+    }
+
+    driver.DriverUnload = Some(driver_exit);
+
+    STATUS_SUCCESS
+}
+
+pub extern "system" fn driver_exit(driver: &mut DRIVER_OBJECT) {
+    unsafe {
+        DbgPrint("Bye, unloading Rust!\0".as_ptr());
+    }
 }
